365266e9bed19ed2e9600c450525d0e419ac8284
Heng-Yi,Chien (ian) <hengyi_chien@summer-time-studio.com>
2018-06-12 22:52:57 +0800

    世界地圖節點系統基礎概念

--- [UnityProject]HeroNeverDie/Assets/Scenes/Ian/Scripts/GroundNodeSystem/GroundNodeManager.cs
+++ [UnityProject]HeroNeverDie/Assets/Scenes/Ian/Scripts/GroundNodeSystem/GroundNodeManager.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundNodeManager : MonoBehaviour {
+public class GroundNodeManager : Singleton<GroundNodeManager>
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public List<GroundNode> AllGroundNodeList;
+
+
+	public void Initialize()
+	{
+		GroundNode[] temp = FindObjectsOfType<GroundNode>();
+		for (int i = 0; i < temp.Length; i++)
+		{
+			AllGroundNodeList.Add(temp[i]);
+		}
+
+		foreach (GroundNode node in AllGroundNodeList)
+		{
+			node.Initialize();
+		}
+	}
+
+	public void UpdateAllGroundNode()
+	{
+		foreach (GroundNode node in AllGroundNodeList)
+		{
+			node.UpdateNodeEffect();
+		}
+		foreach (GroundNode node in AllGroundNodeList)
+		{
+			node.UpdateNodeState();
+		}
+	}
+
+	private void Start ()
+	{
+		Initialize();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	private void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			UpdateAllGroundNode();
+		}
 	}
 }
